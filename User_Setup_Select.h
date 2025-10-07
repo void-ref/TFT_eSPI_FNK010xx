d412846148427ab3df933f2ce787447115b435c9
Mitch <65527685+mtmk-ee@users.noreply.github.com>
2025-10-06 23:42:43 -1000

    Fix broken path

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -6,6 +6,6 @@
     "`User_Setup_Select.h` was #include'd, but `USER_SETUP_LOADED` isn't #define'd"
 #endif
 
-#include "TFT_eSPI_FNK010xx/TFT_eSPI_FNK010xx.h"
+#include "include/TFT_eSPI_FNK010xx/TFT_eSPI_FNK010xx.h"
 
 #endif /* _USER_SETUP_SELECT_H */
