4c740ba81f207b210bcbc18e58deafe35281a6c7
Mitch <65527685+mtmk-ee@users.noreply.github.com>
2025-10-06 23:39:40 -1000

    Shake up the inclusion tree

---
+++ User_Setup_Select.h
@@ -0,0 +1,11 @@
+#ifndef _USER_SETUP_SELECT_H
+#define _USER_SETUP_SELECT_H
+
+#ifndef USER_SETUP_LOADED
+#warning \
+    "`User_Setup_Select.h` was #include'd, but `USER_SETUP_LOADED` isn't #define'd"
+#endif
+
+#include "TFT_eSPI_FNK010xx/TFT_eSPI_FNK010xx.h"
+
+#endif /* _USER_SETUP_SELECT_H */
